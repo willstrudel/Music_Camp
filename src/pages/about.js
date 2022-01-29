f2a9ca2e41cb4ebf8d7f2139433cbaffff8876c9
Will Struder <80079439+willstrudel@users.noreply.github.com>
2022-01-29 02:55:33 -0500

    Finished touching up Pages for the night, font sizing and positioning elements

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function About() {
   return (
     <main>
-      <section className='pt-10 justify-center grid grid-col p-6 font-Jost font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
-        <h1>ABOUT</h1>
+      <section className='pt-10 justify-center grid grid-col font-Jost font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
+        <h1 className="uppercase">About</h1>
       </section>
-      <section className='pt-8 justify-center grid grid-col p-6 font-Jost font-bold'>
+      <section className='top-60 justify-center grid grid-col p-6 font-Jost font-bold relative'>
           <div className='border-2 p-4 border-stone-700 shadow-xl text-lg sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>
             <h3>What: Adult Music Camp</h3>
             <h3>When: 6/15/2023 - 6/18/2023</h3>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Contact Page</h2>
-    </main>
+    <section className='pt-10 justify-center grid grid-col p-6 font-Jost font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
+      <h1 className="uppercase">Contact</h1>
+    </section>
   );
 }
